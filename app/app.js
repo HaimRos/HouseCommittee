d2b51b3d10d8fde6f9391bb246a48f94d9609033
Haim <haimros@gmail.com>
2018-07-27 16:00:15 +0300

    adding home.html

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var app = angular.module("HouseCommitteeApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/home/home.html"
+    })
+    .when("/login", {
+        templateUrl: "app/login/login.html",
+        controller: "loginCtrl"
+    })
+    .when("/signup", {       
+    })
+});
