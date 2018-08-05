9f66094f213ca223568e32eb754511012eaca5f1
Haim <haimros@gmail.com>
2018-08-05 13:20:34 +0300

    not admin log in do not see tenants nav menu item

--- app/navbar/navbarCtrl.js
+++ app/navbar/navbarCtrl.js
@@ -3,7 +3,9 @@ app.controller("navbarCtrl", function ($scope, user, $location) {
     $scope.isUserLoggedIn = function () {
         return user.isLoggedIn();
     }
-
+    $scope.isUserAdmin = function () {
+        return user.isAdmin();
+    }
     $scope.logout = function () {
         user.logout();
         $location.path("/");
--- app/signUp/signUpCtrl.js
+++ app/signUp/signUpCtrl.js
@@ -1,17 +1,6 @@
 app.controller("signUpCtrl", function ($scope, user, $location) {
 
-    $scope.email = "";
-    $scope.password = "";
-    $scope.invalidLogin = false;
 
-    $scope.login = function () {
-        $scope.invalidLogin = false;
-        user.login($scope.email, $scope.password).then(function (activeUser) {
-            $location.path("/dashboard");
-        }, function () {
-            $scope.invalidLogin = true;
-        })
-    }
 
 
 })
