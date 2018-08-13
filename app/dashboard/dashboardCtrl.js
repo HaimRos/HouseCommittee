155868a456d452b60d1caf3c11a50716217b67e8
haimros <haimros@gmail.com>
2018-08-13 13:19:39 +0300

    added number of messages, issues and voting to dashboard Hagasha 1.1

--- app/dashboard/dashboardCtrl.js
+++ app/dashboard/dashboardCtrl.js
@@ -3,27 +3,30 @@ app.controller("dashboardCtrl", function ($scope, user, $rootScope) {
     $rootScope.activeUser = user.getActiveUser();
     $rootScope.isAdmin = user.isAdmin();
 
-     user.getMemberMessageArr().then(function (result){        
-         $scope.messageArr=result;
-     }, function (error) {
-             $log.error(error);
-      });
-
-      user.getMemberIssueArr().then(function (result){        
-         $scope.issueArr=result;
-     }, function (error) {
-             $log.error(error);
-      });
-
-      user.getMemberVotingArr().then(function (result){        
-         $scope.votingsArr=result;
-     }, function (error) {
-             $log.error(error);
-      });
-
-      user.getTenantsArr().then(function (result){        
-        $scope.tenantsArr=result;
+    user.getMemberMessageArr().then(function (result) {
+        $scope.messageArr = result;
+        $scope.numOfMessages = $scope.messageArr.length;
     }, function (error) {
-            $log.error(error);
-     });
+        $log.error(error);
+    });
+
+    user.getMemberIssueArr().then(function (result) {
+        $scope.issueArr = result;
+        $scope.numOfIssues = $scope.issueArr.length;
+    }, function (error) {
+        $log.error(error);
+    });
+
+    user.getMemberVotingArr().then(function (result) {
+        $scope.votingsArr = result;
+        $scope.numOfVotings = $scope.votingsArr.length;
+    }, function (error) {
+        $log.error(error);
+    });
+
+    user.getTenantsArr().then(function (result) {
+        $scope.tenantsArr = result;
+    }, function (error) {
+        $log.error(error);
+    });
 })
